416060f80846cb1c5f3db84623dab8a1ac7f9d8a
lakaganth <lakaganth89@gmail.com>
2021-03-03 16:19:46 -0500

    Initial commit

---
+++ src/pages/servicesPage/ServicesPage.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import ServiceHero from "../../assets/service_hero.png";
+import { Container, ServiceHeroContainer } from "./ServiceStyles";
+import { ServiceBoxPage } from "./ServiceBox";
+import ScrollAnimation from "react-animate-on-scroll";
+import { Footer } from "../../components/UI/Footer";
+
+export const ServicesPage = () => {
+  return (
+    <Container>
+      <ScrollAnimation animateIn="slideInDown">
+        <ServiceHeroContainer>
+          <ScrollAnimation animateIn="slideInLeft" delay={600}>
+            <div className="content">
+              <p>Our Services</p>
+              <p>
+                Our range of services support key stages of your supply chain
+                process, especially in the crucial final mile. While HV
+                Logistics majorly focuses on the final mile, we have dedicated
+                teams handling first mile and mid mile requirements.
+              </p>
+            </div>
+          </ScrollAnimation>
+          <ScrollAnimation animateIn="slideInRight" delay={600}>
+            <div className="hero-img">
+              <img src={ServiceHero} alt="HV Logistics Services" />
+            </div>
+          </ScrollAnimation>
+        </ServiceHeroContainer>
+      </ScrollAnimation>
+      <ServiceBoxPage />
+      <Footer />
+    </Container>
+  );
+};
